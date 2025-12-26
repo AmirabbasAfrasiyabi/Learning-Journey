75f79e3c0dd9122fd3675a4931646879dd957a45
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-12-26 15:12:41 +0330

    days4 created(module)

---
+++ python/days4/days4.py
@@ -0,0 +1,61 @@
+"""integer random module"""
+from pkgutil import resolve_name
+
+# import math
+# import my_module
+# import random
+
+# random_integer = random.randint(1, 100)
+
+# print(random_integer)
+# print(my_module.pi)
+
+"""float random module"""
+# import random
+# random_float = random.random()
+# print(random_float)
+
+
+"""tail and head game with random module"""
+
+# import random
+# random_side = random.randint(0, 1)
+# if random_side == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+
+"""list"""
+# a = [1, 2, 3]
+# print(a)
+# print(len(a))
+#
+# x = a.append(12)
+# print(a)
+# print(len(a))
+
+
+"""list example for Iran state"""
+# stete_of_Iran = ["Ilam" , "Tehran" , "Karaj" , "rasht" , "sari" , "yazd" , "tabriz" , "Ardabil" , "Ahvaz" , "Mashhad" , "Semnan" , "Qom" , "Isfahan"  ]
+#
+# print(stete_of_Iran[3])
+# print(stete_of_Iran[-1])
+#
+# stete_of_Iran[3] = "Rasht"
+# stete_of_Iran[4] = "Sari"
+# stete_of_Iran[5] = "Yazd"
+# stete_of_Iran[6] = "Tabriz"
+# print(stete_of_Iran)
+#
+# stete_of_Iran.append("Kerman")
+# print(stete_of_Iran)
+#
+# stete_of_Iran.extend(["Zanjan" , "Qazvin"])
+# print(stete_of_Iran)
+#
+# """indexError"""
+# num_of_stats = len(stete_of_Iran)
+# print(num_of_stats)
+# print(stete_of_Iran[num_of_stats-1])
+
