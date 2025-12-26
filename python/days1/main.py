eaa4774e7b8323abd3abbee549ea726edb366ebc
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-12-26 15:06:33 +0330

    days1 created and work with print function and all type of print variable

---
+++ python/days1/main.py
@@ -0,0 +1,35 @@
+"""print 3 hello world"""
+# # print("hello world! \nhello world! \nhello world!")
+
+
+"""print hello name"""
+# # name = input()
+# # print("hello" +" "+name)
+
+
+"""print functions"""
+# # name = input("what is your name ->")
+# # age = input("what is your age ->")
+# # # print("hello " + name  + "how old are you?" + "I am "+age +"years old")
+# # print (f'hello {name} how are you . how old are you ?Iam {age} years olds')
+
+
+
+
+"""variable"""
+# name = input("whats your name?")
+# lenght = len(name)
+# print(lenght)
+# print(name)
+
+
+"""project"""
+print("Welcome to the Band Name Generator!")
+
+city= input("Which city did you grow up in?")
+print(city)
+
+pet = input("What is the name of pet?\n")
+print(f"Your band name could be {city} , {pet}")
+
+
